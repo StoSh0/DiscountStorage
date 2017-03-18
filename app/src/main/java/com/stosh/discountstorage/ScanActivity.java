4912ffa6bdb03cba1ddaa1bfbde9ae9cf4127abf
Ruslan Stosyk <stosh.lv.ua@gmail.com>
2017-03-18 04:17:04 +0200

    I want sleep, I create xml schema, Scan and Generate Bitmap with code

--- app/src/main/java/com/stosh/discountstorage/ScanActivity.java
+++ app/src/main/java/com/stosh/discountstorage/ScanActivity.java
@@ -1,8 +1,6 @@
 package com.stosh.discountstorage;
 
 import android.app.Activity;
-import android.app.Fragment;
-import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
@@ -10,6 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.MultiFormatWriter;
@@ -20,22 +19,25 @@ import com.google.zxing.integration.android.IntentResult;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
 
 
-public class ScannerActivity extends AppCompatActivity {
+public class ScanActivity extends AppCompatActivity {
 
     private String TAG = "SCAN";
 
-    Button button;
+    private IntentResult result;
+    private ImageView imageView;
+    private Button button;
+    private Button butto1;
+    private Button butto2;
+    private BitMatrix bitMatrix;
+    private Bitmap bitmap;
+    private BarcodeEncoder barcodeEncoder;
 
-    BitMatrix bitMatrix;
-    Bitmap bitmap;
-    BarcodeEncoder barcodeEncoder;
 
     @Override
     public void onCreate(Bundle state) {
         super.onCreate(state);
-        setContentView(R.layout.activity_scanner);
-
-        button = (Button) findViewById(R.id.button4);
+        setContentView(R.layout.activity_scan);
+init();
         final Activity activity = this;
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,29 +56,36 @@ public class ScannerActivity extends AppCompatActivity {
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
         if (result != null) {
             if (result.getContents() == null) {
                 Log.d(TAG, "Cancelled scan");
                 finish();
             } else {
                 Log.d(TAG, "Scanned");
-                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+
                 try {
-                    bitMatrix = multiFormatWriter.encode(result.getContents(), BarcodeFormat.EAN_13, 200,200);
+                    bitMatrix = new MultiFormatWriter().encode(result.getContents(), BarcodeFormat.EAN_13, 400,200);
                     barcodeEncoder = new BarcodeEncoder();
                     bitmap = barcodeEncoder.createBitmap(bitMatrix);
                 } catch (WriterException e) {
                     e.printStackTrace();
                 }
-                FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-                Fragment fragment = new GenerateFragment();
-                fragmentTransaction.replace(R.id.containerGenerate, fragment).commit();
 
+                imageView.setVisibility(View.VISIBLE);
+                butto1.setVisibility(View.VISIBLE);
+                butto2.setVisibility(View.VISIBLE);
+                imageView.setImageBitmap(bitmap);
             }
         } else {
-            // This is important, otherwise the result will not be passed to the fragment
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void init(){
+        button = (Button) findViewById(R.id.btnScan);
+        butto1 = (Button) findViewById(R.id.btnCancel);
+        butto2 = (Button) findViewById(R.id.btnAdd);
+        imageView = (ImageView) findViewById(R.id.imageViewBarcode);
     }
 }
